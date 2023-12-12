127bd332c92fb3f4240744a034a11297fe56bdad
Gabriel Gontijo <96489350+Gonteejo@users.noreply.github.com>
2023-12-12 20:53:34 -0300

    Estilizando a Sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="http://github.com/gonteejo.png" />
-    <Titulo fontSize={20}>Gabriel Gontijo</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Gabriel Gontijo</Titulo>
+      <Paragrafo fontSize={16} tipo="secundario">
+        Gonteejo
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Dev front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
